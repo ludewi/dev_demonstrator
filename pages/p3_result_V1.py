5936a9a35e9e426cc20b1f7f71f67aa50953851a
ludewi <delu1019@h-ka.de>
2022-06-21 17:40:26 +0200

    show results on result site

--- pages/p3_result_V1.py
+++ pages/p3_result_V1.py
@@ -7,7 +7,7 @@ from tensorflow.python.keras.models import load_model
 import streamlit as st
 from streamlit_drawable_canvas import st_canvas
 
-from pages.p2_demonstrator_V3 import local_val_score, local_train_acc, fed_train_acc, fed_val_score, local_train_log, fed_train_log
+from pages.p2_demonstrator_V3 import local_train_log, fed_train_log
 
 st.set_page_config(
      page_title="Results",
@@ -19,17 +19,9 @@ st.set_page_config(
 st.title("Ergebnisseite")
 st.sidebar.markdown("# Ergebnisseite")
 
-if len(local_val_score) != 0:
-    df_val_temp = pd.DataFrame(data=local_val_score, columns=["Local Accuracy per Round (eval)"])
-    df_fit_temp = pd.DataFrame(data=local_train_acc, columns=["Local Accuracy per Round (fit)"])
-
-if len(fed_train_acc) != 0:
-    df_fed_fit_temp = pd.DataFrame(data=fed_train_acc, columns=["Federated Accuracy per Round (fit)"]) #columns=["Ep1", "Ep2", "Ep3", "Ep4", "Ep5", "Ep6", "Ep7", "Ep8", "Ep9", "Ep10"]
-    df_fed_val_temp = pd.DataFrame(data=fed_val_score, columns=["Federated Accuracy per Round (eval)"])
-
-    result = pd.concat([df_val_temp, df_fit_temp, df_fed_fit_temp, df_fed_val_temp], axis=1)
-    st.dataframe(result)
-    st.line_chart(result)
+if st.session_state["result"] is not st.session_state["result"].empty:
+    st.dataframe(st.session_state["result"])
+    st.line_chart(st.session_state["result"])
 
 else:
     st.write("Es wurde noch kein Training durchgeführt!")
